911298662d50ab0480486356f98eb5a72eaffb37
David-Szczepanik <Davidszczepanik@seznam.cz>
2024-08-09 19:18:14 +0200

    +bullet

--- bullet.gd
+++ bullet.gd
@@ -13,4 +13,14 @@ func _ready():
 
 
 func _process(_delta):
-	position += velocity * delta
+	position += velocity * _delta
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited():
+	queue_free()
+
+
+func _on_body_entered(body):
+	if body.is_in_group("rocks"):
+		body.explode()
+		queue_free()
